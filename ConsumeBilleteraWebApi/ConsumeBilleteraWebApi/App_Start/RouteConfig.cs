fcaf3de2e4cd90c54024f425d67d9cf40dc07862
Alvaro Salvador Valdivia Calizaya <asvaldivia12@gmail.com>
2019-09-06 23:15:27 -0600

    Configuraciones

--- ConsumeBilleteraWebApi/ConsumeBilleteraWebApi/App_Start/RouteConfig.cs
+++ ConsumeBilleteraWebApi/ConsumeBilleteraWebApi/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ConsumeBilleteraWebApi
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Billeteras", action = "Balance", id = UrlParameter.Optional }
             );
         }
     }
